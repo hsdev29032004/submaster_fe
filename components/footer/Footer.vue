cba941e9a888789861f66c0245f694d244d17c59
Do phuc Hung <hsdev.29032004@gmail.com>
2025-04-20 20:55:08 +0700

    refactor: separate header and footer components

---
+++ components/footer/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+    <footer class="border-t border-[#ddd] dark:border-[#1f2937]">
+        <UContainer class="flex justify-between">
+            <p>@Submaster - Bilingual subtitles from any youtube video</p>
+            <p>Version: 0.0.1</p>
+        </UContainer>
+    </footer>
+</template>
