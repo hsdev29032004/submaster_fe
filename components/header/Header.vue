454c92c43f8efa8bb509455a69d639462c98bf95
Do Phuc Hung <hsdev.29032004@gmail.com>
2025-04-21 01:34:29 -0700

    chore: test deploy

--- components/header/Header.vue
+++ components/header/Header.vue
@@ -6,10 +6,10 @@
                     <img src="/images/logo.png" alt="logo" width="120px" />
                 </NuxtLink>
                 <div class="flex items-center">
-                    <!-- <UNavigationMenu color="neutral" variant="link" :items="items" class="w-full" /> -->
+                    <UNavigationMenu color="neutral" variant="link" :items="items" class="w-full" />
 
                     <USwitch v-model="isDark" class="ml-4" unchecked-icon="i-lucide-moon" checked-icon="i-lucide-sun" />
-                    <!-- <UDropdownMenu :items="itemsdropdown" :ui="{
+                    <UDropdownMenu :items="itemsdropdown" :ui="{
                         content: 'w-48',
                         item: 'text-[#62748e]',
                         label: 'text-[#62748e]'
@@ -17,7 +17,7 @@
                         <UButton color="neutral" class="bg-transparent hover:bg-transparent">
                             <UAvatar src="https://github.com/benjamincanac.png" />
                         </UButton>
-                    </UDropdownMenu> -->
+                    </UDropdownMenu>
                 </div>
             </UContainer>
         </div>
@@ -38,76 +38,76 @@ watch(isDark, (val:any) => {
 })
 
 
-// const items = ref([
-//     [
-//         {
-//             label: 'Home',
-//             icon: 'material-symbols:house-outline',
-//             to: '#home',
-//         },
-//         {
-//             label: 'Features',
-//             icon: 'material-symbols:extension-outline-rounded',
-//             to: '#features'
-//         },
-//         {
-//             label: 'Pricing',
-//             icon: 'material-symbols:attach-money-rounded',
-//             to: '#pricing'
-//         }
-//     ]
-// ])
+const items = ref([
+    [
+        {
+            label: 'Home',
+            icon: 'material-symbols:house-outline',
+            to: '#home',
+        },
+        {
+            label: 'Features',
+            icon: 'material-symbols:extension-outline-rounded',
+            to: '#features'
+        },
+        {
+            label: 'Pricing',
+            icon: 'material-symbols:attach-money-rounded',
+            to: '#pricing'
+        }
+    ]
+])
 
-// const itemsdropdown = ref([
-//     [
-//         {
-//             label: 'Benjamin',
-//             avatar: {
-//                 src: 'https://github.com/benjamincanac.png'
-//             },
-//             type: 'label'
-//         }
-//     ],
-//     [
-//         {
-//             label: 'Home',
-//             icon: 'material-symbols:house-outline',
-//             to: '#home',
-//         },
-//         {
-//             label: 'Features',
-//             icon: 'material-symbols:extension-outline-rounded',
-//             to: '#features'
-//         },
-//         {
-//             label: 'Pricing',
-//             icon: 'material-symbols:attach-money-rounded',
-//             to: '#pricing'
-//         }
-//     ],
-//     [
-//         {
-//             label: 'Theme',
-//             icon: 'i-lucide-users'
-//         },
-//         {
-//             label: 'Language',
-//             icon: 'ic:baseline-language',
-//             children: [
-//                 [
-//                     {
-//                         label: 'English',
-//                     },
-//                     {
-//                         label: 'Vietnam',
-//                     }
-//                 ]
-//             ]
-//         },
-//         {
-//             label: 'Logout',
-//             icon: 'ic:baseline-logout',
-//         }
-//     ]
-// ])
+const itemsdropdown = ref([
+    [
+        {
+            label: 'Benjamin',
+            avatar: {
+                src: 'https://github.com/benjamincanac.png'
+            },
+            type: 'label'
+        }
+    ],
+    [
+        {
+            label: 'Home',
+            icon: 'material-symbols:house-outline',
+            to: '#home',
+        },
+        {
+            label: 'Features',
+            icon: 'material-symbols:extension-outline-rounded',
+            to: '#features'
+        },
+        {
+            label: 'Pricing',
+            icon: 'material-symbols:attach-money-rounded',
+            to: '#pricing'
+        }
+    ],
+    [
+        {
+            label: 'Theme',
+            icon: 'i-lucide-users'
+        },
+        {
+            label: 'Language',
+            icon: 'ic:baseline-language',
+            children: [
+                [
+                    {
+                        label: 'English',
+                    },
+                    {
+                        label: 'Vietnam',
+                    }
+                ]
+            ]
+        },
+        {
+            label: 'Logout',
+            icon: 'ic:baseline-logout',
+        }
+    ]
+])
 </script>
