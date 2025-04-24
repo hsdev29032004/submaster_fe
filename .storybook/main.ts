bfb0f454c95856d430cfea5ff2060ede784a69b1
Do Phuc Hung <hsdev.29032004@gmail.com>
2025-04-24 01:44:47 -0700

    chore: test deploy sb

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,26 @@
-import type { StorybookConfig } from '@storybook-vue/nuxt';
+import type { StorybookConfig } from '@storybook-vue/nuxt'
 
 const config: StorybookConfig = {
-  "stories": [
-    "../components/**/*.mdx",
-    "../components/**/*.stories.@(js|jsx|ts|tsx|mdx)"
-  ],
-  "addons": [
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test"
-  ],
-  "framework": {
-    "name": "@storybook-vue/nuxt",
-    "options": {}
-  }
-};
-export default config;
+    stories: [
+        '../components/**/*.mdx',
+        '../components/**/*.stories.@(js|jsx|ts|tsx|mdx)',
+    ],
+    addons: [
+        '@storybook/addon-essentials',
+        '@chromatic-com/storybook',
+        '@storybook/experimental-addon-test',
+    ],
+    framework: {
+        name: '@storybook-vue/nuxt',
+        options: {},
+    },
+    viteFinal: async (config) => {
+        if (!config.resolve) config.resolve = {}
+        if (!config.resolve.alias) config.resolve.alias = {}
+
+        config.resolve.alias['reka-ui'] = '/dev/null'
+
+        return config
+    },
+}
+export default config
