b3e4718e7590ca079960c21e28c8f4daaf045140
Do Phuc Hung <hsdev.29032004@gmail.com>
2025-04-24 20:03:39 -0700

    fix: edit task

--- components/Task/TaskCard.vue
+++ components/Task/TaskCard.vue
@@ -1,116 +1,167 @@
 <template>
-  <li
-    class="text-center flex justify-between pl-2 pr-2"
-    v-if="!(index === idxPick && groupPick === type)"
-    v-show="new RegExp(filter, 'i').test(item)"
-    @dblclick="() => handleDblClick(index, type, item)"
-  >
-    <p
-      v-if="type !== TodoStatus.TODO"
-      @click="() => handleMove(index, type, 'prev', item)"
+    <Modal
+        title="Eaera modal"
+        v-model:visible="visible"
+        :onOk="handleOk"
+        :onCancel="handleCancel"
     >
-      {{ "<" }}
-    </p>
-    <p v-else></p>
-    <p>{{ item }} - <span class="text-red-500 cursor-pointer" @click="() => handleDelete(index, type)">Xóa</span></p>
-    <p
-      v-if="type !== TodoStatus.DONE"
-      @click="() => handleMove(index, type, 'next', item)"
+        <form>
+            <div>
+                <label for="title">Title</label>
+                <Input
+                    type="text"
+                    size="sm"
+                    name="title"
+                    id="title"
+                    fullWidth
+                    v-model="valueEdit.title"
+                />
+            </div>
+            <div class="mt-3">
+                <label for="title">Description</label>
+                <Input
+                    type="text"
+                    size="sm"
+                    name="desc"
+                    id="desc"
+                    fullWidth
+                    v-model="valueEdit.description"
+                />
+            </div>
+        </form>
+    </Modal>
+    <li
+        class="text-center flex justify-between pl-2 pr-2"
+        v-show="new RegExp(filter, 'i').test(item)"
+        @dblclick="() => handleDblClick(index, type, item)"
     >
-      {{ ">" }}
-    </p>
-    <p v-else></p>
-  </li>
-  <Input
-    v-else
-    size="sm"
-    fullWidth
-    v-model="valueEdit"
-    @blur="handleBlur"
-    @keyup.enter="handleBlur"
-    class="block"
-    type="text"
-  />
+        <p
+            v-if="type !== TodoStatus.TODO"
+            @click="() => handleMove(index, type, 'prev', item)"
+        >
+            {{ '<' }}
+        </p>
+        <p v-else></p>
+        <p>
+            {{ item }} -
+            <span
+                class="text-red-500 cursor-pointer"
+                @click="() => handleDelete(index, type)"
+                >Xóa</span
+            >
+        </p>
+        <p
+            v-if="type !== TodoStatus.DONE"
+            @click="() => handleMove(index, type, 'next', item)"
+        >
+            {{ '>' }}
+        </p>
+        <p v-else></p>
+    </li>
 </template>
 
 <script setup lang="ts">
-import { TodoStatus } from '~/stores/todo';
+import { TodoStatus, type Task } from '~/stores/todo'
 
-const idxPick = ref<number>(-1);
-const groupPick = ref<TodoStatus | "">("");
-const valueEdit = ref<string>("");
+const visible = ref(false)
+const idxPick = ref<number>(-1)
+const groupPick = ref<TodoStatus | ''>('')
+const valueEdit = reactive<Task>({
+    title: '',
+    description: '',
+    createdAt: new Date(),
+})
 
 const props = defineProps<{
-  type: TodoStatus;
-  store: any;
-  item: any,
-  index: number
-  filter: string
-}>();
+    type: TodoStatus
+    store: any
+    item: any
+    index: number
+    filter: string
+}>()
 
-const handleDblClick = (index: number, group: TodoStatus, item: string) => {
-  idxPick.value = index;
-  groupPick.value = group;
-  valueEdit.value = item;
-};
+const handleDblClick = (index: number, group: TodoStatus, item: Task) => {
+    idxPick.value = index
+    groupPick.value = group
 
-const handleBlur = () => {
-  if (
-    idxPick.value !== null &&
-    groupPick.value !== null &&
-    valueEdit.value !== null
-  ) {
-    props.store?.edit(idxPick.value, valueEdit.value, groupPick.value);
-    idxPick.value = -1;
-    groupPick.value = TodoStatus.TODO;
-    valueEdit.value = "";
-  }
-};
+    valueEdit.title = item.title
+    valueEdit.description = item.description
+    valueEdit.createdAt = item.createdAt
+
+    openModal()
+}
 
 const handleDelete = (index: number, type: string) => {
-  props.store?.remove(index, type)
+    props.store?.remove(index, type)
 }
 
 const handleMove = (
-  index: number,
-  coloumn: TodoStatus,
-  type: "prev" | "next",
-  item: string
+    index: number,
+    coloumn: TodoStatus,
+    type: 'prev' | 'next',
+    item: string
 ) => {
-  if (type === "prev") {
-    switch (coloumn) {
-      case TodoStatus.TODO: {
-        console.log("Error");
-        break;
-      }
-      case TodoStatus.INPROGRESS: {
-        props.store?.remove(index, coloumn);
-        props.store?.insert(item, TodoStatus.TODO);
-        break;
-      }
-      case TodoStatus.DONE: {
-        props.store?.remove(index, coloumn);
-        props.store?.insert(item, TodoStatus.INPROGRESS);
-        break;
-      }
+    if (type === 'prev') {
+        switch (coloumn) {
+            case TodoStatus.TODO: {
+                console.log('Error')
+                break
+            }
+            case TodoStatus.INPROGRESS: {
+                props.store?.remove(index, coloumn)
+                props.store?.insert(item, TodoStatus.TODO)
+                break
+            }
+            case TodoStatus.DONE: {
+                props.store?.remove(index, coloumn)
+                props.store?.insert(item, TodoStatus.INPROGRESS)
+                break
+            }
+        }
+    } else if (type === 'next') {
+        switch (coloumn) {
+            case TodoStatus.TODO: {
+                props.store?.remove(index, coloumn)
+                props.store?.insert(item, TodoStatus.INPROGRESS)
+                break
+            }
+            case TodoStatus.INPROGRESS: {
+                props.store?.remove(index, coloumn)
+                props.store?.insert(item, TodoStatus.DONE)
+                break
+            }
+            case TodoStatus.DONE: {
+                console.log('Error')
+                break
+            }
+        }
     }
-  } else if (type === "next") {
-    switch (coloumn) {
-      case TodoStatus.TODO: {
-        props.store?.remove(index, coloumn);
-        props.store?.insert(item, TodoStatus.INPROGRESS);
-        break;
-      }
-      case TodoStatus.INPROGRESS: {
-        props.store?.remove(index, coloumn);
-        props.store?.insert(item, TodoStatus.DONE);
-        break;
-      }
-      case TodoStatus.DONE: {
-        console.log("Error");
-        break;
-      }
+}
+
+const handleOk = () => {
+    if (
+        idxPick.value !== null &&
+        groupPick.value !== null &&
+        valueEdit.title?.trim()
+    ) {
+        const newTask: Task = {
+            title: valueEdit.title,
+            description: valueEdit.description,
+            createdAt: valueEdit.createdAt,
+        }
+        props.store?.edit(idxPick.value, newTask, groupPick.value)
+        idxPick.value = -1
+        valueEdit.title = ''
+        valueEdit.description = ''
     }
-  }
-};
+    handleCancel()
+}
+
+const handleCancel = () => {
+    visible.value = false
+}
+
+const openModal = () => {
+    visible.value = true
+}
 </script>
